7dd4e8202f1197892e9c6dfc842f4b539ab16a6c
sara-grinshtein <sry3038@gmail.com>
2025-12-06 22:18:56 +0200

    the feature is working

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const OpenAI = require('openai');
 const nodemailer = require('nodemailer');
+const { error } = require('console');
 require('dotenv').config();
 
 const app = express();
@@ -133,7 +134,7 @@ app.get('/api/schools', (req, res) => {
 // 📚 קבלת תלמידים לפי סמינר
 app.get('/api/school/:schoolId/students', (req, res) => {
   const schoolId = req.params.schoolId;
-  console.log("schoolId is: ",schoolId);
+  console.log("schoolId is: ", schoolId);
   try {
     const users = require('./data/users.json'); // נניח שיש לך קובץ students.json
     const filteredUsers = users.filter(user => user.schoolId === schoolId);
@@ -146,19 +147,31 @@ app.get('/api/school/:schoolId/students', (req, res) => {
 // 🔑 התחברות לפי קוד בית ספר ושם משתמש
 app.post('/api/login', (req, res) => {
   const { schoolCode, username } = req.body;
-  
-  // לוגיקה לבדוק את הקוד ושם המשתמש 
-  // (במקום זאת, זה דוגמה בלבד, יש להוסיף את הבדיקה לפי המידע שלך)
-    const schools = require('./data/schools.json'); // נניח שיש לך קובץ schools.json
-    const school = schools.find(s=>s.code === schoolCode );
-    const users = require('./data/users.json'); // נניח שיש לך קובץ students.json
-     const user = users.find(u=>u.name == username );
-  if (school && user) {
-    res.json({ success: true, message: 'Login successful', user:user }); 
-  } else {
-    res.status(400).json({ error: 'Invalid school code or username' });
+  try 
+  {
+    const  schools  = require('./data/schools.json'); // נניח שיש לך קובץ schools.json
+    const school = schools.find(s => s.code === schoolCode);
+
+    const { users } = require('./data/users.json');
+    const user = users.find(u =>
+      u.name == username && u.schoolCode === school.code);
+    if (school && user) {
+      res.json({
+        success: true,
+        message: 'Login successful',
+        user
+      })
+    }
+    return res.status(400).json({
+      error: 'Invalid school code or username'
+    })
   }
-});
+  catch (error) {
+    console.log("Login error: ", error);
+    res.status(500).json({ error: 'Login failed' });
+  }
+}
+);
 
 // 📁 שיתוף קבצי וידאו סטטיים
 app.use('/videos', express.static('public/videos'));
@@ -173,5 +186,5 @@ app.get("*", (req, res) => {
 
 
 // const PORT = process.env.PORT || 5000;
-const PORT =  5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
